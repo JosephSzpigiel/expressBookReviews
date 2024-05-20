21b7738b280fbcde18febe0acd561a582a3174c5
Joseph Szpigiel <josephszpigiel@gmail.com>
2024-05-20 13:27:43 -0400

    Completed CRUD

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,8 +5,29 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
+const doesExist = (username)=>{
+  let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
 public_users.post("/register", (req,res) => {
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+    if (!doesExist(username)) {
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});
+    }
+  }
+  return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
@@ -25,20 +46,37 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author.toLowerCase()
+  const isbns= Object.keys(books)
+  const results = {}
+
+  isbns.forEach(isbn => {
+    if(books[isbn].author.toLowerCase().replace(/ /g, '_').includes(author)){
+      results[isbn] = books[isbn]
+    }
+  });
+  return res.status(200).json({results});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title.toLowerCase()
+  const isbns= Object.keys(books)
+  const results = {}
+
+  isbns.forEach(isbn =>{
+    if(books[isbn].title.toLowerCase().replace(/ /g, '_').includs(title)){
+      results[isbn] = books[isbn]
+    }
+  })
+  return res.status(200).json(results);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn
+  const book = books[isbn]
+  return res.status(200).json({'results': book['reviews']});
 });
 
 module.exports.general = public_users;
